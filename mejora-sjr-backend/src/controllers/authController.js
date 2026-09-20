5d756b7de3bac9f4d0099fd2858756b98575ff70
Eloscar21 <abrahamgarcia0321@gmail.com>
2026-09-19 19:28:04 -0600

    chore: remove folders with incorrect db schema

--- mejora-sjr-backend/src/controllers/authController.js
+++
@@ -1,7 +0,0 @@
-const loginTest = (req, res) => {
-  res.json({ mensaje: 'Endpoint de login conectado' });
-};
-
-module.exports = {
-  loginTest
-};
--- mejora-sjr-backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { loginTest } = require('../controllers/authController');
-
-// Endpoint de prueba: GET /login
-router.get('/login', loginTest);
-
-module.exports = router;
